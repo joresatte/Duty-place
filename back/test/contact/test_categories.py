065c18cb47495b0a39f3fcae810472e741017918
joresatte <joresatte@yahoo.fr>
2022-03-12 08:47:58 +0100

    test categories, returning list of categories

--- back/test/contact/test_categories.py
+++ back/test/contact/test_categories.py
@@ -1,3 +1,5 @@
+from cgitb import text
+from unicodedata import category
 from src.lib.utils import temp_file
 from src.webserver import create_app
 from src.domain.categories import CategoriesRepository, Categories
@@ -10,41 +12,34 @@ def test_should_return_empty_list_categories():
     response = client.get("/api/categories")
     assert response.json == []
 
-# def test_should_return_list_of_contacts_by_user():
-#     contact_repository = CategoriesRepository(temp_file())
-#     app = create_app(repositories={"contact": contact_repository})
-#     client = app.test_client()
+def test_should_return_list_of_categories():
+    categories_repository = CategoriesRepository(temp_file())
+    app = create_app(repositories={"categories": categories_repository})
+    client = app.test_client()
 
-#     joseba = Contact(
-#         id= "contact-1",
-#         user_id= "joseba_1",
-#         first_name= "Jores",
-#         last_name= "Vince",
-#         email= "joresatte@exemple.com",
-#         phone= "62654662656"
-#     )
-#     contact_repository.save(joseba)
-#     josu = Contact(
-#         id= "contact-2",
-#         user_id= "josu_1",
-#         first_name= "Ibrahim ",
-#         last_name= "Diomande",
-#         email= "ibrahim@exemple.com",
-#         phone= "62654662657"
-#     )
-#     contact_repository.save(josu)
+    category_1 = Categories(
+        cat_id= "category_1",
+        text= "Mudanzas",
+       
+    )
+    categories_repository.save(category_1)
+
+    category_2 = Categories(
+        cat_id= "category_2",
+        text= "Limpiezas",
+    )
+    categories_repository.save(category_2)
     
-#     response = client.get("/api/contact", headers={
-#         'Authorization':'joseba_1'
-#         })
-#     assert response.json == [{
-#         "id": "contact-1",
-#         "user_id": "joseba_1",
-#         "first_name": "Jores",
-#         "last_name": "Vince",
-#         "full_name": "Jores Vince",
-#         "email": "joresatte@exemple.com",
-#         "phone": "62654662656"
-#     },
-#     ]
+    response = client.get("/api/categories")
+    assert response.json == [{
+        "cat_id": "category_1",
+        "text": "Mudanzas",
+        
+    },
+    {
+        "cat_id": "category_2",
+        "text": "Limpiezas",
+        
+    },
+    ]
 
