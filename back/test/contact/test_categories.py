45901414e41345937d1a55b016fcffdf61953f20
joresatte <joresatte@yahoo.fr>
2022-03-12 08:27:48 +0100

    test categories, returning emty list of categories

--- back/test/contact/test_categories.py
+++ back/test/contact/test_categories.py
@@ -2,7 +2,7 @@ from src.lib.utils import temp_file
 from src.webserver import create_app
 from src.domain.categories import CategoriesRepository, Categories
 
-def test_should_return_empty_categories():
+def test_should_return_empty_list_categories():
     Categories_Repository = CategoriesRepository(temp_file())
     app = create_app(repositories={"categories": Categories_Repository})
     client = app.test_client()
