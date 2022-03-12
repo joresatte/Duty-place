6e8e5e8cdf6780fdad47d242e1fcbf11166266df
joresatte <joresatte@yahoo.fr>
2022-03-12 08:59:26 +0100

    printing categories data in initial data

--- back/scripts/initial_data.py
+++ back/scripts/initial_data.py
@@ -1,43 +1,33 @@
 import sys
-from unicodedata import name
 
 sys.path.insert(0, "")
 
-from domain.categories import CategoriesRepository, Contact
-from src.domain.users import UserRepository, User
+from src.domain.categories import CategoriesRepository, Categories
 
 database_path = "data/database.db"
 
-user_repository = UserRepository(database_path)
-user_repository.save(
-   User( 
-        id= "user-1",
-        name= "joseba"
+category_1 = Categories(
+        cat_id= "category_1",
+        text= "Mudanzas",
+       
     )
+category_2 = Categories(
+    cat_id= "category_2",
+    text= "Limpiezas",
+    
 )
-user_repository.save(
-    User(
-        id= "user-2",
-        name= "josu"
+category_3 = Categories(
+        cat_id= "category_3",
+        text= "Cuidados",
+       
     )
-)
-
-joseba = Contact(
-    id= "contact-1",
-    user_id= "user-1",
-    first_name= "Jores",
-    last_name= "Vince",
-    email= "joresatte@exemple.com",
-    phone= "62654662656"
-)
-josu = Contact(
-    id= "contact-2",
-    user_id= "user-2",
-    first_name= "Ibrahim",
-    last_name= "Diomande",
-    email= "ibrahim@exemple.com",
-    phone= "62654662657"
-)
+category_4 = Categories(
+        cat_id= "category_4",
+        text= "Mantenimientos",
+       
+    )
 contact_repository = CategoriesRepository(database_path)
-contact_repository.save(joseba)
-contact_repository.save(josu)
+contact_repository.save(category_1)
+contact_repository.save(category_2)
+contact_repository.save(category_3)
+contact_repository.save(category_4)
