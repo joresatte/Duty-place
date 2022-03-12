c4bbc4603424f91a8001aca90b3bd38d571b26fd
joresatte <joresatte@yahoo.fr>
2022-03-12 09:41:10 +0100

    setting homepage.vue

--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <section class="home">
+    {{welcome}}
+  </section>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
+  // components: {
+  //   HelloWorld
+  // },
+  data(){
+    return{
+      welcome:"welcome to Duty-Place"
+    }
+  },
 }
 </script>
