fa9b986403fc74076da53baddf3b225c2735eb24
joresatte <joresatte.joresatte@yahoo.fr>
2022-03-14 13:36:36 +0100

    still working in ServicesByCategoryPage merge with the back end

--- front/src/pages/services/byCategory/ServicesByCategoryPage.vue
+++ front/src/pages/services/byCategory/ServicesByCategoryPage.vue
@@ -1,21 +1,29 @@
 <template>
   <h1>Services By Category Page</h1>
   <p>{{category_id}} </p>
+  <section>
+ <h2>{{services.user_name}}</h2>
+  <p>{{services.phone}}</p>
+  <p>{{services.email}}</p>
+  <p>{{services.city}}</p>
+  </section>
+
 </template>
 
 <script>
+import config from "@/config.js";
 export default {
   name: 'ServicesByCategoryPage',
   props:['category_id'],
   data(){
     return{
-      categoryServices: [],
+      services: [],
     }
   },
   async mounted(){
     await fetch(`${config.route_Path}/services/by-category/` + this.category_id)
     .then(res => res.json())
-    .then(data => this.categoryServices = data)
+    .then(data => this.services = data)
     .catch(err=> console.log(err.message))
   }
 
