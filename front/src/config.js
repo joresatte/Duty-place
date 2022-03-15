3feb61787d9e704aefef02ef103cce0c59fadf42
joresatte <joresatte@yahoo.fr>
2022-03-15 19:51:08 +0100

    fix set up config.js

--- front/src/config.js
+++ front/src/config.js
@@ -1,3 +1,3 @@
 export default{
-    router_Path:'http://localhost:5000/api',
+    api_Path:'http://localhost:5000/api',
 }
--- front/src/pages/apiservices/api.js
+++ front/src/pages/apiservices/api.js
@@ -1,14 +1,14 @@
 import config from "@/config.js";
 
 export async function getCategories(){
-    return fetch(`${config.router_Path}/categories`)
+    return fetch(`${config.api_Path}/categories`)
             .then(res => res.json())
             .catch(err=> console.log(err.message))
 
 }
 
 export async function getCategoriesServices(category_id){
-    return fetch(`${config.router_Path}/services/by-category/${category_id}`)
+    return fetch(`${config.api_Path}/services/by-category/${category_id}`)
             .then(res => res.json())
             .catch(err=> console.log(err.message))
 }
