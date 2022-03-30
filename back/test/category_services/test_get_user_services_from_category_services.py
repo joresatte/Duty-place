81fe42fcbe63f3d4dd7f712baf939ff26709c4a6
joresatte <joresatte.joresatte@yahoo.fr>
2022-03-30 10:57:14 +0200

    still working on test_get_user_services_category_services.py

--- back/test/category_services/test_get_user_services_from_category_services.py
+++ back/test/category_services/test_get_user_services_from_category_services.py
@@ -1,37 +1,43 @@
 from src.lib.utils import temp_file
 from src.webserver import create_app
-from src.domain.category_services import CategoryServicesRepository, Category_services
+from src.domain.users_services import ServicesRepository, Services
 
 def test_should_return_services_list_by_category_id():
-    services_repository = CategoryServicesRepository(temp_file())
+    services_repository = ServicesRepository(temp_file())
     app = create_app(repositories={"services": services_repository})
     client = app.test_client()
 
-    user_1= Category_services(
+    user_1= Services(
        id= "service_1",
         cat_id= "category_1",
         user_name= "vince",
         text= "Mudanzas",
+        text1= "Mudanzas",
+        textarea= "Mudanzas",
         phone= "024-639-2574",
         email= "reinabo@vince.com",
         city= "Bilbao",
     )
     services_repository.save(user_1)
     
-    user_2= Category_services(
+    user_2= Services(
         id= "service_2",
         cat_id= "category_2",
         user_name= "oshulem0",
         text= "Limpiezas",
+        text1= "Mudanzas",
+        textarea= "Mudanzas",
         phone= "424-639-9574",
         email= "fbadland0@bizjournals.com",
         city= "Tayirove",
     )
-    user_3= Category_services(
+    user_3= Services(
         id= "service_50",
         cat_id= "category_2",
         user_name= "zegerton1",
         text= "Limpiezas",
+        text1= "Mudanzas",
+        textarea= "Mudanzas",
         phone= "382-214-1560",
         email= "wglenister1@latimes.com",
         city= "Al Burayqah",
@@ -39,7 +45,7 @@ def test_should_return_services_list_by_category_id():
     services_repository.save(user_2)
     services_repository.save(user_3)
     
-    response = client.get("/api/services/user_services/service_1")
+    response = client.get("/api/services/user_services")
     assert response.json == [{
         "id": "service_1",
         "cat_id": "category_1",
