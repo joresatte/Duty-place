1ca5d7c6cf294cde85d183faf1f39a25a773e3bb
joresatte <joresatte@yahoo.fr>
2022-04-17 22:50:13 +0200

    /

--- front/src/config.js
+++ front/src/config.js
@@ -1,6 +1,7 @@
 export default{
     api_Path:'http://localhost:5000/api',
     regist_Path:'http://localhost:5000/api',
-    login_Path:'http://localhost:5000/api', 
+    login_Path:'http://localhost:5000/api',
+    userService_Path:'http://localhost:5000/api/services', 
 
 }
